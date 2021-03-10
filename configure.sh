b686dad6d502af835fe72bbe7beeb4eef964b282
Guilherme Oliveira <guigm.oliveira@gmail.com>
2021-03-10 13:06:16 +0100

    Update configure.sh to Release mode

--- configure.sh
+++ configure.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+rm -rf build
 mkdir -p build
 cd build
 
 path="${HOME}/Desktop/GPTool"
 
-cmake -DCMAKE_INSTALL_PREFIX=$path ..
+cmake -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=$path ..
 
 make -j install
